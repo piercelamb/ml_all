df7c2eaf89445932408f5b2fffd9d300df51f080
Pierce Lamb <pierce@flickerbox.com>
2021-08-31 08:10:16 -0700

    WIP

--- supervised_learning/main.py
+++ supervised_learning/main.py
@@ -1,5 +1,6 @@
 import sys
 import pandas as pd
+import numpy as np
 import matplotlib
 matplotlib.use("TkAgg")
 import matplotlib.pyplot as plt
@@ -16,9 +17,22 @@ from sklearn.metrics import plot_confusion_matrix
 from imblearn.pipeline import Pipeline
 from imblearn.over_sampling import SMOTE
 from imblearn.under_sampling import RandomUnderSampler
+from sklearn.model_selection import learning_curve
 from collections import Counter
+import time
+from functools import wraps
 
 RANDOM_STATE = 1337
+
+def timeit(func):
+    @wraps(func)
+    def timed_function(*args, **kwargs):
+        start = time.time()
+        output = func(*args, **kwargs)
+        end = time.time()
+        print('%s execution time: %f secs' % (func.__name__, end - start))
+        return output
+    return timed_function
 
 def plot_accuracy_scores(ccp_alphas, train_scores, test_scores, figure):
     fig, ax = plt.subplots()
@@ -33,31 +47,33 @@ def plot_accuracy_scores(ccp_alphas, train_scores, test_scores, figure):
     plt.savefig(figure)
     plt.clf()
 
-def plot_learning_curve(results):
-    test_scores = results['mean_test_score']
-    train_scores = results['mean_train_score']
+def plot_learning_curve(model,title,X_train,y_train,cv=None):
+    plt.figure()
+    plt.title(title)
+    plt.xlabel("Num Samples")
+    plt.ylabel("Accuracy")
+    step=np.linspace(1/cv,1.0,cv)
+    train_sizes,train_scores,test_scores = learning_curve(model,X_train,y_train,cv=cv,train_sizes=step)
+    test_scores_mean = np.mean(test_scores, axis=1)
+    test_scores_std = np.std(test_scores, axis=1)
+    train_scores_mean = np.mean(train_scores, axis=1)
+    train_scores_std = np.std(train_scores, axis=1)
+    plt.grid()
+    plt.fill_between(train_sizes, test_scores_mean - test_scores_std, test_scores_mean + test_scores_std, alpha=0.1,
+                     color='red')
+    plt.fill_between(train_sizes,train_scores_mean-train_scores_std,train_scores_mean+train_scores_std,alpha=0.1,color='purple')
+    plt.plot(train_sizes,test_scores_mean,'o-',color='red',label="Cross-validation Score")
+    plt.plot(train_sizes,train_scores_mean,'o-',color='purple',label="Training Score")
+    plt.legend()
+    plt.savefig('OSI_Learning_Curve.png')
+    return plt
 
-    plt.plot(test_scores, label='test')
-    plt.savefig('test_learning_curve.png')
-    plt.clf()
-    plt.plot(train_scores, label='train')
-    plt.savefig('train_learning_curve.png')
-    plt.clf()
-
-def get_cross_validated_clf(pipeline, X_train, y_train):
-    parameters_to_tune = {
-        'model__max_depth': range(3, 20),
-        'model__criterion': ('gini', 'entropy'),
-        'model__ccp_alpha': [0.0, 0.000005, 0.00005, 0.0005, 0.005],
-        'model__min_samples_split':range(2,10),
-        'model__min_samples_leaf':range(1,5),
-        #'model__max_features': ['auto', 'sqrt', 'log2'],
-        'model__random_state':[RANDOM_STATE]
-    }
+@timeit
+def get_cross_validated_clf(pipeline, parameters_to_tune, X_train, y_train, cv):
     clf = GridSearchCV(
         pipeline,
         param_grid=parameters_to_tune,
-        cv=5,
+        cv=cv,
         #verbose=3,
         n_jobs=-1)
     clf.fit(X=X_train, y=y_train)
@@ -65,13 +81,11 @@ def get_cross_validated_clf(pipeline, X_train, y_train):
     # [('over', SMOTE(sampling_strategy=0.3)), ('under', RandomUnderSampler(sampling_strategy=0.5)),
     #  ('model', DecisionTreeClassifier(ccp_alpha=0.0005, criterion='entropy', max_depth=5,
     #                                   min_samples_split=4, random_state=1337))]
-    tree_model = clf.best_estimator_.steps[2][1]
+    #tree_model = clf.best_estimator_.steps[2][1]
+    tree_model = clf.best_estimator_
     print(clf.best_score_, clf.best_params_)
     print('Accuracy Score on cross-validated DT: ' + str(clf.best_score_))
     print('Size of cross-validated DT: ' + str(tree_model.tree_.node_count))
-    #pd.DataFrame(clf.cv_results_).to_csv('dt_cross_validation_results.csv')
-    #plot_learning_curve(clf.cv_results_)
-    print(clf.cv_results_)
     return tree_model, clf.best_params_
 
 def get_pruned_clf(tree_model, best_params, X_train, X_test, y_train, y_test):
@@ -102,20 +116,34 @@ def get_pruned_clf(tree_model, best_params, X_train, X_test, y_train, y_test):
     plot_accuracy_scores(plotted_alphas, train_scores, test_scores, 'figure_2.png')
     return max_score, potential_clfs[max_score]
 
-def find_optimal_clf(pipeline, X_train, X_test, y_train, y_test):
-    tree_model, best_params = get_cross_validated_clf(pipeline, X_train, y_train)
+@timeit
+def get_max_score(clf, X_test, y_test):
+    return clf.score(X_test, y_test)
+
+def find_optimal_clf(pipeline, parameters_to_tune, cv, X_train, X_test, y_train, y_test):
+    tree_model, best_params = get_cross_validated_clf(pipeline, parameters_to_tune, X_train, y_train, cv)
     #max_score, optimal_clf = get_pruned_clf(tree_model, best_params, X_train, X_test, y_train, y_test)
-    max_score = tree_model.score(X_test, y_test)
+    max_score = get_max_score(tree_model, X_test, y_test)
     return max_score, tree_model
 
-#tried 0.3, 0.5 (89.6%)
+#tried 0.3, 0.5 (89.6%), 0.4, 0.5 (89.8), 0.4, 0.7 (89.2), 0.5/0.8 (87), 0.5/0.5 (90) 0.6/0.6 (87) 0.4/0.4 (89.91)
 def get_imbalanced_data_pipeline():
     model = dt.DecisionTreeClassifier()
-    over = SMOTE(sampling_strategy=0.3)
-    under = RandomUnderSampler(sampling_strategy=0.5)
+    over = SMOTE(sampling_strategy=0.5, random_state=RANDOM_STATE)
+    under = RandomUnderSampler(sampling_strategy=0.5, random_state=RANDOM_STATE)
     steps = [('over', over), ('under', under), ('model', model)]
     pipeline = Pipeline(steps=steps)
-    return pipeline
+
+    parameters_to_tune = {
+        'model__max_depth': range(3, 20),
+        'model__criterion': ('gini', 'entropy'),
+        'model__ccp_alpha': [0.0, 0.000005, 0.00005, 0.0005, 0.005],
+        # 'model__min_samples_split':range(2,10),
+        # 'model__min_samples_leaf':range(1,5),
+        # 'model__max_features': ['sqrt', 'log2', None],
+        'model__random_state':[RANDOM_STATE]
+    }
+    return pipeline, parameters_to_tune
 
 def run_shoppers(dataroot):
     datapath = dataroot + 'online_shoppers_intention.csv'
@@ -124,10 +152,10 @@ def run_shoppers(dataroot):
     target = df['Revenue']
     attributes = df.drop('Revenue', axis=1,)
     string_columns = ['Month', 'VisitorType', 'Weekend']
-    numerical_columns = ['Administrative', 'Administrative_Duration', 'Informational',
-       'Informational_Duration', 'ProductRelated', 'ProductRelated_Duration',
-       'BounceRates', 'ExitRates', 'PageValues', 'SpecialDay',
-       'OperatingSystems', 'Browser', 'Region', 'TrafficType',]
+    # numerical_columns = ['Administrative', 'Administrative_Duration', 'Informational',
+    #    'Informational_Duration', 'ProductRelated', 'ProductRelated_Duration',
+    #    'BounceRates', 'ExitRates', 'PageValues', 'SpecialDay',
+    #    'OperatingSystems', 'Browser', 'Region', 'TrafficType',]
     #TODO: maybe can eek more performance out by passing numerical columns through RobustScalar (remove passthrough)
     #TODO: https://stackoverflow.com/a/52801019
     #TODO: https://scikit-learn.org/stable/modules/generated/sklearn.preprocessing.RobustScaler.html
@@ -139,12 +167,22 @@ def run_shoppers(dataroot):
     clean_attrs = column_trans.fit_transform(attributes)
     #TODO: we could maybe eek more accuracy out of the test_size param
     #TODO: https://scikit-learn.org/stable/modules/generated/sklearn.model_selection.train_test_split.html
-    imbalanced_data_pipeline = get_imbalanced_data_pipeline()
-
+    #imbalanced_data_pipeline, parameters_to_tune = get_imbalanced_data_pipeline()
+    imbalanced_data_pipeline = dt.DecisionTreeClassifier()
+    parameters_to_tune = {
+        'max_depth': range(3, 20),
+        'criterion': ('gini', 'entropy'),
+        'ccp_alpha': [0.0, 0.000005, 0.00005, 0.0005, 0.005],
+        # 'model__min_samples_split':range(2,10),
+        # 'model__min_samples_leaf':range(1,5),
+        # 'model__max_features': ['sqrt', 'log2', None],
+        'random_state': [RANDOM_STATE]
+    }
     X_train, X_test, y_train, y_test = train_test_split(clean_attrs, target, stratify=target, test_size=0.3, random_state=RANDOM_STATE)
     #print(Counter(y_train))
     #exit(1)
-    max_score, optimal_clf = find_optimal_clf(imbalanced_data_pipeline, X_train, X_test, y_train, y_test)
+    cv = 10
+    max_score, optimal_clf = find_optimal_clf(imbalanced_data_pipeline, parameters_to_tune, cv, X_train, X_test, y_train, y_test)
     print('Accuracy Score on test Data: '+str(max_score))
     confusion_matrix = plot_confusion_matrix(
         optimal_clf,
@@ -156,8 +194,7 @@ def run_shoppers(dataroot):
     confusion_matrix.ax_.set_title('Confusion Matrix for predictions')
     plt.savefig('OSI_confusion_matrix.png')
     plt.clf()
-    exit(1)
-
+    plot_learning_curve(optimal_clf, 'Decision Tree Learning Curve', X_train, y_train, cv=cv)
 
 def run_ford(dataroot):
     df_train = pd.read_csv(dataroot + 'fordTrain.csv')
@@ -168,6 +205,7 @@ def run_ford(dataroot):
 
 
     #TODO: do feature selection with SFS from this article:
+    #https://scikit-learn.org/0.24/modules/generated/sklearn.feature_selection.SequentialFeatureSelector.html
     #https://towardsdatascience.com/5-feature-selection-method-from-scikit-learn-you-should-know-ed4d116e4172
 if __name__ == "__main__":
     passed_arg = sys.argv[1]
