8e585e6aaa5a5406cede1fd6231e8b8ab25bafcf
Pierce Lamb <pierce@flickerbox.com>
2021-09-12 09:15:26 -0700

    WIP

--- supervised_learning/clean.py
+++ supervised_learning/clean.py
@@ -76,7 +76,7 @@ def get_data_ford():
     X_test = df_test[['P6', 'E9', 'V10']]
     return X_train, y_train, X_test, y_test
 
-def plot_learning_curve(model,title,X_train,y_train,cv, filename,scoring):
+def plot_learning_curve(is_iterative, model,title,X_train,y_train,cv, filename,scoring):
     plt.figure()
     plt.title(title)
     plt.xlabel("Num Samples")
@@ -95,7 +95,7 @@ def plot_learning_curve(model,title,X_train,y_train,cv, filename,scoring):
     plt.plot(train_sizes,train_scores_mean,'o-',color='purple',label="Training Score")
     plt.legend()
     plt.savefig(filename)
-    return plt
+    plt.clf()
 
 
 def get_confusion_matrix(optimal_clf, X_test, y_test, title, filename):
@@ -136,18 +136,33 @@ def plot_validation_curve(run_type, clf_type, param, score, score_training, scor
     plt.savefig(run_type+'_'+smote_text+'_'+clf_type + '_' +param + '_' + score + '_validation_curve.png')
     plt.clf()
 
-def final_plots(run_type, clf, clf_type, cv, score, X_train, y_train, X_test, y_test, y_pred, smote):
+def plot_loss_curve(model, title, filename):
+    plt.xlabel("Num Iterations")
+    plt.ylabel("Loss")
+    plt.title(title)
+    plt.plot(model.loss_curve_)
+    plt.savefig(filename)
+    plt.clf()
+
+def final_plots(run_type, clf, clf_type, cv, score, X_train, y_train, X_test, y_test, y_pred, smote, is_iterative):
     smote_text = "SMOTE" if smote[0] else 'reg'
     print(clf_type + " on scoring method " + score + " Accuracy score: " + str(accuracy_score(y_test, y_pred)))
     print(clf_type + " on scoring method " + score + " f1 score: " + str(f1_score(y_test, y_pred)))
     get_confusion_matrix(clf, X_test, y_test, run_type+' '+smote_text+' Confusion Matrix (' + clf_type + ')',
                          run_type+'_'+smote_text+'_'+clf_type + '_' + score + '_confusion_matrix.png')
-    plot_learning_curve(clf, run_type+' '+smote_text+' Learning Curve (' + clf_type + ')', X_train, y_train, cv=cv, scoring=score,
+    plot_learning_curve(is_iterative, clf, run_type+' '+smote_text+' Learning Curve (' + clf_type + ')', X_train, y_train, cv=cv, scoring=score,
                         filename=run_type+'_'+smote_text+'_'+clf_type + '_' + score + '_Learning_Curve.png')
+    if is_iterative:
+        plot_loss_curve(clf, run_type+' '+smote_text+' Loss Curve (' + clf_type + ')',
+                        run_type+'_'+smote_text+'_'+clf_type + '_' + score + '_Loss_Curve.png')
 
-def initialize_a_param(clf, param, X_train, y_train, cv, clf_type, scoring):
+def initialize_a_param(clf, param, X_train, y_train, cv, clf_type, scoring, is_smote):
     param_name = list(param)[0]
     print("Initializing "+param_name+" with GridSearch")
+    if is_smote:
+        smote_param_name = "model__" + param_name
+        param[smote_param_name] = param.pop(param_name)
+        param_name = smote_param_name
     clf = GridSearchCV(
         clf,
         param_grid=param,
@@ -193,9 +208,9 @@ def get_classifier(clf_type, param_name, option, param2, smote):
     if param2 != None:
         param2_name = list(param2)[0]
         param2_val = param2[param2_name]
-        kwargs = {param_name: option, 'random_state': RANDOM_STATE, param2_name: param2_val, 'n_jobs':-1}
+        kwargs = {param_name: option, 'random_state': RANDOM_STATE, param2_name: param2_val}
     else:
-        kwargs = {param_name: option, 'random_state': RANDOM_STATE, 'n_jobs':-1}
+        kwargs = {param_name: option, 'random_state': RANDOM_STATE}
 
     return discover_classifier(clf_type, kwargs, smote)
 
@@ -220,6 +235,10 @@ def calc_validation_curve(clf_type, score, cv, X_train, y_train, X_test, y_test,
 
 def get_validation_curve(run_type, score, cv, clf_type, X_train, y_train, X_test, y_test, param1, param2, smote):
     param_name = list(param1)[0]
+    if smote[0] and param_name.startswith('model__'):
+        fixed_param_name = param_name[len('model__'):]
+        param1[fixed_param_name] = param1.pop(param_name)
+        param_name = fixed_param_name
     param_options = param1[param_name]
     print('\n----------------------\n')
     print('Executing validation curve for '+param_name)
@@ -234,7 +253,7 @@ def get_validation_curve(run_type, score, cv, clf_type, X_train, y_train, X_test
 def validate_two_params(param1, param2, clf, run_type, clf_type, scoring, cv,X_train, y_train, X_test, y_test, smote):
     param2_name = list(param2)[0]
     param1_name = list(param1)[0]
-    param_2_initial_val = initialize_a_param(clf, param2, X_train, y_train, cv, clf_type, scoring)
+    param_2_initial_val = initialize_a_param(clf, param2, X_train, y_train, cv, clf_type, scoring, smote[0])
     param2_initialized = {param2_name: param_2_initial_val}
     best_param1_value = get_validation_curve(run_type, scoring, cv, clf_type, X_train, y_train, X_test, y_test,
                                              param1, param2_initialized, smote)
@@ -252,14 +271,14 @@ def validate_two_params(param1, param2, clf, run_type, clf_type, scoring, cv,X_t
         if param2_best[list(param2_best)[0]] != best_param2_value:
             print("2nd CV iteration value: " + str(param2_best[list(param2_best)[0]]) + " != best value:" + str(best_param2_value))
 
-    kwargs = {param1_name: best_param1_value, param2_name: best_param2_value, 'random_state': RANDOM_STATE, 'n_jobs':-1}
+    kwargs = {param1_name: best_param1_value, param2_name: best_param2_value, 'random_state': RANDOM_STATE}
     return kwargs
 
 def validate_single_param(param1, param2, run_type, clf_type, scoring, cv,X_train, y_train, X_test, y_test, smote):
     best_param_value = get_validation_curve(run_type, scoring, cv, clf_type, X_train, y_train, X_test, y_test, param1,
                                             param2, smote)
     param_name = list(param1)[0]
-    kwargs = {param_name: best_param_value, 'random_state': RANDOM_STATE, 'n_jobs':-1}
+    kwargs = {param_name: best_param_value, 'random_state': RANDOM_STATE}
     return kwargs
 
 def get_optimal_hyperparameters(param2, clf_type, smote, param1, run_type, scoring, cv,X_train,y_train,X_test,y_test):
@@ -273,51 +292,56 @@ def get_optimal_hyperparameters(param2, clf_type, smote, param1, run_type, scori
 
 def run_dt(X_train, y_train, X_test, y_test, scoring, cv, smote, run_type, param1, param2):
     clf_type = 'decision_tree'
+    is_iterative = False
     kwargs = get_optimal_hyperparameters(param2, clf_type, smote, param1, run_type, scoring, cv,X_train,y_train,X_test,y_test)
     clf = dt.DecisionTreeClassifier(**kwargs)
     y_pred = calc_testing(clf, X_train, y_train, X_test)
-    final_plots(run_type, clf, clf_type, cv, scoring, X_train, y_train, X_test, y_test, y_pred, smote)
+    final_plots(run_type, clf, clf_type, cv, scoring, X_train, y_train, X_test, y_test, y_pred, smote, is_iterative)
 
 
 def run_ada(X_train, y_train, X_test, y_test, scoring, cv, smote, run_type, param1, param2):
     clf_type = 'ada_boost'
+    is_iterative = False
     kwargs = get_optimal_hyperparameters(param2, clf_type, smote, param1, run_type, scoring, cv, X_train, y_train,
                                          X_test, y_test)
     clf = AdaBoostClassifier(**kwargs)
     y_pred = calc_testing(clf, X_train, y_train, X_test)
-    final_plots(run_type, clf, clf_type, cv, scoring, X_train, y_train, X_test, y_test, y_pred, smote)
+    final_plots(run_type, clf, clf_type, cv, scoring, X_train, y_train, X_test, y_test, y_pred, smote, is_iterative)
 
 def run_nn(X_train, y_train, X_test, y_test, scoring, cv, smote, run_type, param1, param2):
     clf_type = 'neural_network'
+    is_iterative = True
     kwargs = get_optimal_hyperparameters(param2, clf_type, smote, param1, run_type, scoring, cv, X_train, y_train,
                                          X_test, y_test)
     clf = MLPClassifier(**kwargs)
     y_pred = calc_testing(clf, X_train, y_train, X_test)
-    final_plots(run_type, clf, clf_type, cv, scoring, X_train, y_train, X_test, y_test, y_pred, smote)
+    final_plots(run_type, clf, clf_type, cv, scoring, X_train, y_train, X_test, y_test, y_pred, smote, is_iterative)
 
 def run_svc(X_train, y_train, X_test, y_test, scoring, cv, smote, run_type, param1, param2):
     clf_type = 'svc'
+    is_iterative = True
     kwargs = get_optimal_hyperparameters(param2, clf_type, smote, param1, run_type, scoring, cv, X_train, y_train,
                                          X_test, y_test)
     clf = SVC(**kwargs)
     y_pred = calc_testing(clf, X_train, y_train, X_test)
-    final_plots(run_type, clf, clf_type, cv, scoring, X_train, y_train, X_test, y_test, y_pred, smote)
+    final_plots(run_type, clf, clf_type, cv, scoring, X_train, y_train, X_test, y_test, y_pred, smote, is_iterative)
 
 def run_knn(X_train, y_train, X_test, y_test, scoring, cv, smote, run_type, param1, param2):
     clf_type = 'knn'
+    is_iterative = False
     kwargs = get_optimal_hyperparameters(param2, clf_type, smote, param1, run_type, scoring, cv, X_train, y_train,
                                          X_test, y_test)
     if 'random_state' in kwargs: del kwargs['random_state']
     clf = KNeighborsClassifier(**kwargs)
     y_pred = calc_testing(clf, X_train, y_train, X_test)
-    final_plots(run_type, clf, clf_type, cv, scoring, X_train, y_train, X_test, y_test, y_pred, smote)
+    final_plots(run_type, clf, clf_type, cv, scoring, X_train, y_train, X_test, y_test, y_pred, smote, is_iterative)
 
 
 def run_shoppers(dataroot):
     X_train, y_train, X_test, y_test = get_data_OSI()
     scoring = 'f1'
     cross_val_folds = 10
-    smote = (False,0.2,0.6)
+    smote = (False,0.3,0.6)
     run_type = 'OSI'
     # run_dt(
     #     X_train,
@@ -331,18 +355,6 @@ def run_shoppers(dataroot):
     #     {'max_depth': [1,2,3,4,5,6,7,8,9,10,15,20]},
     #     None
     # )
-    # run_ada(
-    #     X_train,
-    #     y_train,
-    #     X_test,
-    #     y_test,
-    #     scoring,
-    #     cross_val_folds,
-    #     smote,
-    #     run_type,
-    #     {'n_estimators': [1,5,10, 20, 30, 40, 50, 60]},
-    #     {'learning_rate': [0.0001, 0.001, 0.01]}
-    # )
     # run_nn(
     #     X_train,
     #     y_train,
@@ -354,10 +366,12 @@ def run_shoppers(dataroot):
     #     run_type,
     #     #(10,10,10) = 3 hidden layers with 10 units, (10,) = 1 hidden layer with 10 units
     #     #had to play with the below numbers a lot
-    #     {'hidden_layer_sizes': [(100,), (150,), (200,), (100,100,100)]},  # validation_curve
-    #     {'activation': ['logistic', 'tanh', 'relu']},  # validation_curve
+    #     {'hidden_layer_sizes': [(100,), (150,), (200,), (100,100), (100,100,100)]},  # validation_curve
+    #     {'activation': ['logistic', 'tanh', 'relu', 'identity']},  # validation_curve
     # )
-    # run_svc(
+    #Weak learner puts more weight on the examples its getting wrong (showing they're the 'harder' examples)
+    # and as long as you're still getting better than chance error, it guarantees you'll get some of the harder examples right
+    # run_ada(
     #     X_train,
     #     y_train,
     #     X_test,
@@ -366,11 +380,10 @@ def run_shoppers(dataroot):
     #     cross_val_folds,
     #     smote,
     #     run_type,
-    #     {'C': [1,2,3,4,5,6,7,8,9,10]},  # validation_curve
-    #     #{'kernel': ['linear', 'poly', 'rbf', 'sigmoid', 'precomputed']},  # validation_curve
-    #     {'kernel': ['rbf', 'sigmoid']},  # validation_curve
+    #     {'n_estimators': [50, 100, 150, 200]},
+    #     {'learning_rate': [0.0001, 0.001, 0.01]}
     # )
-    run_knn(
+    run_svc(
         X_train,
         y_train,
         X_test,
@@ -379,10 +392,28 @@ def run_shoppers(dataroot):
         cross_val_folds,
         smote,
         run_type,
-        {'n_neighbors': [3,4,5,6,7,8,9,10]},  # validation_curve
-        {'weights': ['uniform', 'distance']},  # validation_curve
+        {'C': [1,2,3,4,5,6,7,8,9,10]},  # validation_curve
+        #{'kernel': ['linear', 'poly', 'rbf', 'sigmoid', 'precomputed']},  # validation_curve
+        {'kernel': ['rbf', 'sigmoid']},  # validation_curve
     )
+    #preference bias
+    #Locality: Near points are similar
+    #smoothness: averaging over nearest neighbors
+    #all features matter eqaully
+    # run_knn(
+    #     X_train,
+    #     y_train,
+    #     X_test,
+    #     y_test,
+    #     scoring,
+    #     cross_val_folds,
+    #     smote,
+    #     run_type,
+    #     {'n_neighbors': [3,4,5,6,7,8,9,10]},  # validation_curve
+    #     {'weights': ['uniform', 'distance']},  # validation_curve
+    # )
 
+#TODO: See if the distribution of trues to falses is the same between training and testing
 def run_ford(dataroot):
     X_train, y_train, X_test, y_test = get_data_ford()
     scoring = 'accuracy'
@@ -401,6 +432,20 @@ def run_ford(dataroot):
     #     {'max_depth': [3,5,9,10,12,14,16,18,20]},
     #     None
     # )
+    # run_nn(
+    #     X_train,
+    #     y_train,
+    #     X_test,
+    #     y_test,
+    #     scoring,
+    #     cross_val_folds,
+    #     smote,
+    #     run_type,
+    #     #(10,10,10) = 3 hidden layers with 10 units, (10,) = 1 hidden layer with 10 units
+    #     #had to play with the below numbers a lot
+    #     {'hidden_layer_sizes': [(100,), (150,), (200,), (100,100), (100,100,100)]},  # validation_curve
+    #     {'activation': ['logistic', 'tanh', 'relu', 'identity']},  # validation_curve
+    # )
     # run_ada(
     #     X_train,
     #     y_train,
@@ -408,8 +453,36 @@ def run_ford(dataroot):
     #     y_test,
     #     scoring,
     #     cross_val_folds,
-    #     smote
+    #     smote,
+    #     run_type,
+    #     {'n_estimators': [50, 100, 150, 200]},
+    #     {'learning_rate': [0.0001, 0.001, 0.01]}
     # )
+    # run_knn(
+    #     X_train,
+    #     y_train,
+    #     X_test,
+    #     y_test,
+    #     scoring,
+    #     cross_val_folds,
+    #     smote,
+    #     run_type,
+    #     {'n_neighbors': [3,4,5,6,7,8,9,10]},  # validation_curve
+    #     {'weights': ['uniform', 'distance']},  # validation_curve
+    # )
+    run_svc(
+        X_train,
+        y_train,
+        X_test,
+        y_test,
+        scoring,
+        cross_val_folds,
+        smote,
+        run_type,
+        {'C': [1,2,3,4,5,6,7,8,9,10]},  # validation_curve
+        #{'kernel': ['linear', 'poly', 'rbf', 'sigmoid', 'precomputed']},  # validation_curve
+        {'kernel': ['rbf', 'sigmoid']},  # validation_curve
+    )
 if __name__ == "__main__":
     passed_arg = sys.argv[1]
     if passed_arg.startswith('/'):
