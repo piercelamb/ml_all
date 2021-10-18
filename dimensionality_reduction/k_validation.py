e67f95f0ba64987ab4ac5b8bd3b4f741a8dd565a
Pierce Lamb <pierce@flickerbox.com>
2021-10-18 05:52:55 -0700

    pushing to run on 2nd comp

--- dimensionality_reduction/k_validation.py
+++ dimensionality_reduction/k_validation.py
@@ -47,6 +47,7 @@ from sklearn.base import ClusterMixin
 from sklearn.mixture import GaussianMixture
 from yellowbrick.cluster import KElbow
 import statistics
+import matplotlib.cm as cm
 
 RANDOM_STATE = 1337
 def get_smote_pipeline(smote):
@@ -82,50 +83,107 @@ def run_clustering_algs(run_type, k_clusters, metrics, X):
         'kmeans',
          'em'
     ]
-    k_results = []
     for alg in clustering_algs:
         print("**************************")
         print("Running "+alg)
+        df = pd.DataFrame(index=k_clusters, columns=metrics)
         for metric in metrics:
-            print("Using metric: "+metric)
-            if alg == 'kmeans':
-                #clusterer = KMeans(n_clusters=k, random_state=RANDOM_STATE)
-                visualizer = KElbowVisualizer(KMeans(random_state=RANDOM_STATE), k=k_clusters, metric=metric)
-            else:
-                #clusterer = mixture.GaussianMixture(n_components=k, random_state=RANDOM_STATE)
-                visualizer = KElbowVisualizer(GaussianMixtureCluster(random_state=RANDOM_STATE), k=k_clusters, metric=metric, force_model=True)
-            print("Fitting "+metric)
-            visualizer.fit(X)
-            best_k = visualizer.elbow_value_
-            best_k_score = visualizer.elbow_score_
-            k_results.append(best_k)
-            print("Metric "+metric+" best k: "+str(best_k)+" with score "+str(best_k_score))
-            visualizer.finalize()
-            visualizer.show(outpath=run_type+"_"+alg+"_"+metric+"_kelbow.png")
-            plt.clf()
+            print("Using metric: "+metric+" across "+str(len(k_clusters))+" different k values")
+            for k in k_clusters:
+                # Create a subplot with 1 row and 2 columns
+                fig, ax1 = plt.subplots()
 
+                # The 1st subplot is the silhouette plot
+                # The silhouette coefficient can range from -1, 1 but in this example all
+                # lie within [-0.1, 1]
+                ax1.set_xlim([-0.1, 1])
+                # The (n_clusters+1)*10 is for inserting blank space between silhouette
+                # plots of individual clusters, to demarcate them clearly.
+                ax1.set_ylim([0, len(X) + (k + 1) * 10])
 
-    data = Counter(k_results)
-    print(k_results)
-    print(data.most_common())  # Returns all unique items and their counts
-    best_k_overall = data.most_common(1)[0][0]  # Returns the highest occurring item
-    print("The most chosen k is: "+str(best_k_overall))
-    for alg in clustering_algs:
-        print("plotting InterClusterDistance for: " + alg)
-        if alg == 'kmeans':
-            model = KMeans(best_k_overall, random_state=RANDOM_STATE)
-            viz = InterclusterDistance(model)
-            viz.fit(X)
-            viz.finalize()
-            viz.show(outpath=run_type + "_" + alg + "_interclusterdistance.png")
-            plt.clf()
-            viz = SilhouetteVisualizer(model)
-            viz.fit(X)
-            viz.finalize()
-            viz.show(outpath=run_type + "_" + alg + "_silhouetteViz.png")
-            plt.clf()
+                # Initialize the clusterer with n_clusters value and a random generator
+                # seed of 10 for reproducibility.
+                if alg == 'kmeans':
+                    clusterer = KMeans(n_clusters=k, random_state=RANDOM_STATE)
+                else:
+                    clusterer = GaussianMixture(n_components=k, random_state=RANDOM_STATE)
+                cluster_labels = clusterer.fit_predict(X)
 
+                # The silhouette_score gives the average value for all the samples.
+                # This gives a perspective into the density and separation of the formed
+                # clusters
+                silhouette_avg = silhouette_score(X, cluster_labels, metric=metric)
+                # print("For n_clusters =", k,
+                #       "The average silhouette_score is :", silhouette_avg)
 
+                df.at[k, metric] = silhouette_avg
+                # Compute the silhouette scores for each sample
+                sample_silhouette_values = silhouette_samples(X, cluster_labels)
+
+                y_lower = 10
+                for i in range(k):
+                    # Aggregate the silhouette scores for samples belonging to
+                    # cluster i, and sort them
+                    ith_cluster_silhouette_values = \
+                        sample_silhouette_values[cluster_labels == i]
+
+                    ith_cluster_silhouette_values.sort()
+
+                    size_cluster_i = ith_cluster_silhouette_values.shape[0]
+                    y_upper = y_lower + size_cluster_i
+
+                    color = cm.nipy_spectral(float(i) / k)
+                    ax1.fill_betweenx(np.arange(y_lower, y_upper),
+                                      0, ith_cluster_silhouette_values,
+                                      facecolor=color, edgecolor=color, alpha=0.7)
+
+                    # Label the silhouette plots with their cluster numbers at the middle
+                    ax1.text(-0.05, y_lower + 0.5 * size_cluster_i, str(i))
+
+                    # Compute the new y_lower for next plot
+                    y_lower = y_upper + 10  # 10 for the 0 samples
+
+                ax1.set_title("The silhouette plot for the various clusters.")
+                ax1.set_xlabel("The silhouette coefficient values")
+                ax1.set_ylabel("Cluster label")
+
+                # The vertical line for average silhouette score of all the values
+                ax1.axvline(x=silhouette_avg, color="red", linestyle="--")
+
+                ax1.set_yticks([])  # Clear the yaxis labels / ticks
+                ax1.set_xticks([-0.1, 0, 0.2, 0.4, 0.6, 0.8, 1])
+
+                # # 2nd Plot showing the actual clusters formed
+                # colors = cm.nipy_spectral(cluster_labels.astype(float) / k)
+                # ax2.scatter(X[:, 0], X[:, 1], marker='.', s=30, lw=0, alpha=0.7,
+                #             c=colors, edgecolor='k')
+                #
+                # # Labeling the clusters
+                # centers = clusterer.cluster_centers_
+                # # Draw white circles at cluster centers
+                # ax2.scatter(centers[:, 0], centers[:, 1], marker='o',
+                #             c="white", alpha=1, s=200, edgecolor='k')
+                #
+                # for i, c in enumerate(centers):
+                #     ax2.scatter(c[0], c[1], marker='$%d$' % i, alpha=1,
+                #                 s=50, edgecolor='k')
+                #
+                # ax2.set_title("The visualization of the clustered data.")
+                # ax2.set_xlabel("Feature space for the 1st feature")
+                # ax2.set_ylabel("Feature space for the 2nd feature")
+
+                plt.suptitle(("Silhouette analysis for KMeans clustering on sample data "
+                              "with n_clusters = %d" % k),
+                             fontsize=14, fontweight='bold')
+
+                plt.savefig('metrics/'+run_type+"_"+alg+"_"+metric+"_"+str(k)+".png")
+                plt.clf()
+                # time.sleep(5)
+                # plt.close('all')
+                # fig.gcf()
+
+        print("Printing average silhouette scores for "+alg)
+        print(df)
 
 
 
@@ -166,8 +224,8 @@ def run_clustering_ford(dataroot, k_clusters, metrics):
 
 def run_clustering(dataroot):
     smote = (False, 0.6)
-    k_clusters = (2,25)
-    metrics = ['distortion', 'silhouette', 'calinski_harabasz']
+    k_clusters = [2,3,4,5]
+    metrics = ['cityblock', 'cosine', 'euclidean', 'l1', 'l2', 'manhattan']
     print("Running clustering on OCI dataset")
     run_clustering_shoppers(dataroot, k_clusters, metrics, smote)
     print("\n----------------------------------\n")
