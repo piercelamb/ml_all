488dd8a6220715814b214b788557d55d685833cc
Pierce Lamb <pierce@flickerbox.com>
2021-10-24 11:28:06 -0700

    WIP

--- dimensionality_reduction/neural_net.py
+++ dimensionality_reduction/neural_net.py
@@ -56,6 +56,16 @@ from itertools import product
 
 RANDOM_STATE = 1337
 
+def timeit(func):
+    @wraps(func)
+    def timed_function(*args, **kwargs):
+        start = time.time()
+        output = func(*args, **kwargs)
+        end = time.time()
+        print('%s execution time: %f secs' % (func.__name__, end - start))
+        return output
+    return timed_function
+
 def scale_continuous_data(X_train, X_test):
     categorical = ['E3', 'E7', 'E8', 'V5']
     numerical = [x for x in X_train.columns if x not in categorical]
@@ -93,10 +103,65 @@ def get_data_ford(dataroot, dataset_sample, is_rfc):
         X_test = X_test.set_axis(range(0, num_features), axis=1)
     return X_train, y_train, X_test, y_test, num_features
 
+@timeit
 def calc_testing(clf, X_train, y_train, X_test):
     clf.fit(X_train, y_train)
     y_pred = clf.predict(X_test)
     return y_pred
+
+def get_confusion_matrix(optimal_clf, X_test, y_test, title, filename):
+    confusion_matrix = plot_confusion_matrix(
+        optimal_clf,
+        X_test,
+        y_test,
+        cmap=plt.cm.Blues,
+        normalize='true'
+    )
+    confusion_matrix.ax_.set_title(title)
+    plt.savefig(filename)
+    plt.clf()
+
+def plot_learning_curve(is_iterative, model,title,X_train,y_train,cv, filename,scoring):
+    plt.figure()
+    plt.title(title)
+    plt.xlabel("Num Samples")
+    plt.ylabel(scoring)
+    step=np.linspace(1/cv,1.0,cv)
+    train_sizes,train_scores,test_scores = learning_curve(model,X_train,y_train,cv=cv,train_sizes=step, scoring=scoring, n_jobs=-1)
+    test_scores_mean = np.mean(test_scores, axis=1)
+    test_scores_std = np.std(test_scores, axis=1)
+    train_scores_mean = np.mean(train_scores, axis=1)
+    train_scores_std = np.std(train_scores, axis=1)
+    plt.grid()
+    plt.fill_between(train_sizes, test_scores_mean - test_scores_std, test_scores_mean + test_scores_std, alpha=0.1,
+                     color='red')
+    plt.fill_between(train_sizes,train_scores_mean-train_scores_std,train_scores_mean+train_scores_std,alpha=0.1,color='purple')
+    plt.plot(train_sizes,test_scores_mean,'o-',color='red',label="Cross-validation Score")
+    plt.plot(train_sizes,train_scores_mean,'o-',color='purple',label="Training Score")
+    plt.legend()
+    plt.savefig(filename)
+    plt.clf()
+
+def plot_loss_curve(model, title, filename):
+    plt.xlabel("Num Iterations")
+    plt.ylabel("Loss")
+    plt.title(title)
+    plt.plot(model.loss_curve_)
+    plt.savefig(filename)
+    plt.clf()
+
+def final_plots(run_type, clf, clf_type, cv, score, X_train, y_train, X_test, y_test, y_pred, alg_name, is_iterative):
+
+    print(clf_type + " on scoring method " + score + " Accuracy score: " + str(accuracy_score(y_test, y_pred)))
+    print(clf_type + " on scoring method " + score + " f1 score: " + str(f1_score(y_test, y_pred)))
+    get_confusion_matrix(clf, X_test, y_test, run_type+' '+alg_name+' Confusion Matrix (' + clf_type + ')',
+                         run_type+'_'+alg_name+'_'+clf_type + '_' + score + '_confusion_matrix.png')
+    plot_learning_curve(is_iterative, clf, run_type+' '+alg_name+' Learning Curve (' + clf_type + ')', X_train, y_train, cv=cv, scoring=score,
+                        filename=run_type+'_'+alg_name+'_'+clf_type + '_' + score + '_Learning_Curve.png')
+    if is_iterative:
+        plot_loss_curve(clf, run_type+' '+alg_name+' Loss Curve (' + clf_type + ')',
+                        run_type+'_'+alg_name+'_'+clf_type + '_' + score + '_Loss_Curve.png')
+
 
 def rfc_determine_components(run_type, X_train, y_train, X_test, y_test, num_features):
     print("Running RFC")
@@ -130,15 +195,59 @@ def rfc_determine_components(run_type, X_train, y_train, X_test, y_test, num_fea
     best_features = best_features_first[:best_feature_length]
     print("Best features: "+ str(best_features))
 
+import warnings
+warnings.filterwarnings("ignore")
+def run_DR(run_type, X_train, y_train, X_test, y_test, kwargs, cv):
+    scoring = 'accuracy'
+    smote = False
+    is_iterative = True
+    DR_algs = {
+        'PCA': PCA(n_components=12, random_state=RANDOM_STATE),
+        'ICA': FastICA(n_components=29, random_state=RANDOM_STATE),
+        'RP': GaussianRandomProjection(n_components=18, random_state=RANDOM_STATE),
+        'RFC': [29,17,5,16,19,13]
+    }
+
+
+    for alg_name, alg in DR_algs.items():
+        print("Running "+alg_name)
+        if alg_name != 'RFC':
+            X_Train_new = alg.fit_transform(X_train)
+            X_test_new = alg.fit_transform(X_test)
+        else:
+            X_Train_new = X_train[alg]
+            X_test_new = X_test[alg]
+
+        print("Starting GridSearch")
+        start = time.time()
+        gs = GridSearchCV(MLPClassifier(), kwargs, cv=cv, n_jobs=-2)
+        gs.fit(X_Train_new, y_train)
+        end = time.time()
+        print('%s execution time: %f secs' % (alg_name, end - start))
+        best_nn = gs.best_estimator_
+        best_score = gs.best_score_
+        best_params = gs.best_params_
+        y_pred = calc_testing(best_nn, X_Train_new, y_train, X_test_new)
+        print(alg_name+" best training score was: "+str(best_score))
+        print(alg_name+" best params: "+str(best_params))
+
+        final_plots(run_type, best_nn, 'NN', cv, scoring, X_Train_new, y_train, X_test_new, y_test, y_pred, alg_name, is_iterative)
+        
+
 def run_nn_DR(dataroot):
     smote = (False, 0.6)
     is_rfc = True
     training_sample = 0.3
     run_type = 'FAD'
+    cv = 5
+    kwargs = {
+        'hidden_layer_sizes':[(100,), (150,), (100,200), (100,200,400)],
+        'activation':['tanh', 'logistic'],
+    }
     X_train, y_train, X_test, y_test, num_features = get_data_ford(dataroot, training_sample, is_rfc)
-    rfc_determine_components(run_type,  X_train, y_train, X_test, y_test, num_features)
+    #rfc_determine_components(run_type,  X_train, y_train, X_test, y_test, num_features)
     print("Running dimensionality reduction on FAD dataset")
-    #
+    run_DR(run_type, X_train, y_train, X_test, y_test, kwargs, cv)
     print("Applying reduced dataset to Neural Net")
 
 if __name__ == "__main__":
